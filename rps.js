9b43f0fcd19ffa1078a6c4ecf79d5a071b23f5cf
Wilbur Summerson <wilbur.summerson@ada.ac.uk>
2024-09-25 13:06:31 +0000

    removed unneeded code

--- rps.js
+++ rps.js
@@ -22,24 +22,9 @@ function rockPaperScissors(player1, player2) {
   } else if ((player1 === "spock") && (player2 === "rock")) {
     return "player1";
   } else {
-    return "player2"
+    return "player2";
   }
 }
-
-function randomChoice() {
-  const randNum = Math.floor(Math.random()*3+1);
-  if (randNum === 1) {
-    return "rock";
-  } else if (randNum === 2) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
-}
-
-const player1Choice = randomChoice();
-const player2Choice = randomChoice();
-console.log(rockPaperScissors(player1Choice, player2Choice));
 
 // Leave this code here for the automated tests
 module.exports = {
