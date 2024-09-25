ddd1b1f7abeb46ec6297b6c7b9559fd9098594e6
Wilbur Summerson <wilbur.summerson@ada.ac.uk>
2024-09-25 13:23:59 +0000

    refactored code using dictionaries containing win conditions

--- rps.js
+++ rps.js
@@ -1,30 +1,19 @@
 function rockPaperScissors(player1, player2) {
+  const winCondition = {
+    rock: ["scissors", "lizards"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["spock", "paper"],
+    spock: ["scissors", "rock"]
+  }
   if (player1 === player2) {
     return "draw";
-  } else if ((player1 === "rock") && (player2 === "scissors")) {
-    return "player1";
-  } else if ((player1 === "rock") && (player2 === "lizard")) {
-    return "player1";
-  } else if ((player1 === "paper") && (player2 === "rock")) {
-    return "player1";
-  } else if ((player1 === "paper") && (player2 === "spock")) {
-    return "player1";
-  } else if ((player1 === "scissors") && (player2 === "paper")) {
-    return "player1";
-  } else if ((player1 === "scissors") && (player2 === "lizard")) {
-    return "player1";
-  } else if ((player1 === "lizard") && (player2 === "spock")) {
-    return "player1";
-  } else if ((player1 === "lizard") && (player2 === "paper")) {
-    return "player1";
-  } else if ((player1 === "spock") && (player2 === "scissors")) {
-    return "player1";
-  } else if ((player1 === "spock") && (player2 === "rock")) {
+  } else if (winCondition[player1].includes(player2)) {
     return "player1";
   } else {
     return "player2";
   }
-}
+} 
 
 // Leave this code here for the automated tests
 module.exports = {
