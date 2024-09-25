e2fc021452f8bbfd50bd31f5d1947b81e30fe7e1
Wilbur Summerson <wilbur.summerson@ada.ac.uk>
2024-09-25 14:02:05 +0000

    final version

--- rps.js
+++ rps.js
@@ -9,16 +9,6 @@ function rockPaperScissors(player1, player2) {
   return player1 === player2 ? 'draw' : winCondition[player1].includes(player2) ? 'player1' : "player2";
 }
 
-
-/*
-if (player1 === player2) {
-    return "draw";
-  } else if (winCondition[player1].includes(player2)) {
-    return "player1";
-  } else {
-    return "player2";
-  }
-    */
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
